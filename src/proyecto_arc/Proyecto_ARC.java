1cb6ab92184c72f5ed1288b7c9a236f483d10123
algorol12 <multimedia@DESKTOP-S6IB766>
2020-10-10 11:11:29 +0200

    hola alber 007

--- src/proyecto_arc/Proyecto_ARC.java
+++ src/proyecto_arc/Proyecto_ARC.java
@@ -17,7 +17,7 @@ public class Proyecto_ARC {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //dsds
+        //Hola alberto fffsss
         
     }
     
